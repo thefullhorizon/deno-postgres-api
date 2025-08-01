72ec01622a611baa809981637aefb3e228e758f1
taylor <thefullhorizon@gmail.com>
2025-08-01 18:03:41 +0800

    调整deno项目的配置和加载方式

--- deps.ts
+++ deps.ts
@@ -1,14 +1,8 @@
-// 管理项目的依赖项
-
-// 由于生产环境是Deno将环境变量加载进运行环境中，使用Deno.env.get()来获得环境变量。
-// 而本地开发时，使用dotenv来加载.env文件中的环境变量。综合考虑两种情况，
-if (Deno.env.get("RAILWAY_ENVIRONMENT") === undefined) {
-  // 本地环境 - 只在本地开发时加载 dotenv
-  await import("https://deno.land/std@0.204.0/dotenv/load.ts");
-}else {
-  // 生产环境-Railway
-}
-
-export { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
-export { Client } from "https://deno.land/x/postgres@v0.19.3/mod.ts";
+// ========用于集中管理项目依赖========
+// 处理本地.env中的环境变量，当导入std/dotenv时，系统会将.env文件中的变量加载到运行时环境变量（Deno.env）中
+import "std/dotenv";
+// 这是Deno项目中用于构建Web应用和API的工具，就像Node.js中的Express一样
+export { Application, Router } from "oak" ;
+// 这是Deno中用于连接PostgreSQL数据库的库
+export { Client } from "postgres";
 
